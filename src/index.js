c7adc8080ee1a4fcef9b5077874dcc508094de42
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2023-02-28 11:30:25 +0530

    async operation

--- src/index.js
+++ src/index.js
@@ -14,19 +14,19 @@ app.post('/users', async (req,res) => {
 
  const user =new User(req.body);
   
-  // try{
-  //   await user.save()
-  //   res.statue(200).send(user)
-  // }catch(e){
-  //   res.status(400).send(e)
-  // }
+  try{
+    await user.save();
+    res.status(200).send(user);
+  }catch(err){
+    res.status(400).send(err);
+  }
 
- user.save().then(() => {
-  res.status(201).send(user)
-  }).catch((err) =>{
-    // console.log(err)
-    res.status(400).send("falied inserting")
-  })
+  // user.save().then(() => {
+  // res.status(201).send(user)
+  // }).catch((err) =>{
+  //   // console.log(err)
+  //   res.status(400).send("falied inserting")
+  // })
 })
 
 app.patch('/users/:id', async (req,res)=>{
@@ -43,62 +43,97 @@ app.patch('/users/:id', async (req,res)=>{
   }
 })
 
-app.get('/users',(req, res)=>{
-  User.find({}).then((user)=>{
-    res.send(user)
-  }).catch((user)=>{
-    res.send("Server down")
-  })
+app.get('/users', async(req, res)=>{
+
+  try{
+    const user = await User.find({})
+    res.status(200).send(user);
+  } catch{
+    res.send(500).send("SERVER DOWN");
+  }
+  // User.find({}).then((user)=>{
+  //   res.send(user)
+  // }).catch((user)=>{
+  //   res.send("Server down")
+  // })
 })
 
-app.get('/users/:id', (req,res)=>{
+app.get('/users/:id', async(req,res)=>{
   // console.log(req.params.id);
   const id=req.params.id;
 
-  User.findById(id).then((user)=>{
-    if(!user)
-    {
-      return res.status(404).send("server down")
-    }
-    res.status(201).send(user) //user found
+  try{
+    const user= await User.findById(id);
+    if(!user)  return res.status(500).send("server down")
+    res.status(200).send(user);
+  }
+  catch{
+    res.status(400).send("Not found")
+  }
+  // User.findById(id).then((user)=>{
+  //   if(!user)
+  //   {
+  //     return res.status(404).send("server down")
+  //   }
+  //   res.status(201).send(user) //user found
 
-  }).catch((err)=>{
-    res.send("user not found in the registered database")
-  })
+  // }).catch((err)=>{
+  //   res.send("user not found in the registered database")
+  // })
 })
 
-app.post('/tasks',(req,res)=>{
+app.post('/tasks',async(req,res)=>{
   const task= new Task(req.body);
-  console.log(req.body)
-  
-  task.save().then(()=>{
-    res.status(201).send("Data Inserted",task);
-  }).catch((err)=>{
-    res.status(400).send("Failed")
-  })
+  // console.log(req.body)
+  try{
+    await task.save()
+    res.status(200).send(task)
+  }catch{
+    res.status(400).send('FAILED')
+  }
+  // task.save().then(()=>{
+  //   res.status(201).send("Data Inserted",task);
+  // }).catch((err)=>{
+  //   res.status(400).send("Failed")
+  // })
 })
 
 
-app.get('/tasks',(req,res)=>{
-  Task.find({}).then((task)=>{
-      res.status(200).send(task);
-  }).catch((err)=>{
-    res.send("Fetching failed")
-  })
+app.get('/tasks',async (req,res)=>{
+
+  try{
+    const task=await Task.find({})
+    res.status(200).send(task)  
+  }catch{
+    res.status(500).send("SERVER DOWN")
+  }
+  // Task.find({}).then((task)=>{
+  //     res.status(200).send(task);
+  // }).catch((err)=>{
+  //   res.send("Fetching failed")
+  // })
 })
 
-app.get('/tasks/:id', (req, res)=>{
+app.get('/tasks/:id', async(req, res)=>{
   const id=req.params.id;
 
-  Task.findById(id).then((task)=>{
-    if(!task)
-    {
-      return res.status(500).send("SERVER DOWN")
-    }
-    res.status(201).send(task);
-  }).catch((err)=>{
-    res.status(400).send("FAILED")
-  })
+  try{
+    const task=await Task.findById(id)
+    if(!task) return res.status(500).send("SERVER DOWN ")
+
+    res.status(200).send(task);
+  } catch{
+    res.status(400).send("Not found")
+  } 
+  // Task.findById(id).then((task)=>{
+  //   if(!task)
+  //   {
+  //     return res.status(500).send("SERVER DOWN")
+  //   }
+  //   res.status(201).send(task);
+  // }).catch((err)=>{
+  //   res.status(400).send("FAILED")
+  // })
 })
 
 
